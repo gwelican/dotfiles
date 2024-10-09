1bd2f425b62cddd1f4e7fc8f442d99c3691b850f
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2024-10-09 16:19:09 -0700

    refactor for darwin nix

--- flake.nix
+++ flake.nix
@@ -1,45 +1,72 @@
 {
-  description = "Home Manager configuration of pvarsanyi";
+  description = "Top level Nix flake";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "flake:nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixpkgs/nixos-24.05";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "flake:home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      macSystem = "aarch64-darwin";
-      linuxSystem = "x86_64-linux";
-      # pkgs = nixpkgs.legacyPackages.${system};
-      homeConfig = {system, username, homeDir}: home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { inherit system; };
-        modules = [
-          ./home.nix
-          ./git/default.nix
-          ./dotfiles/default.nix
-          {
-            home.username = username;
-            home.homeDirectory = homeDir;
-            home.stateVersion = "24.05";
-          }
-        ];
-      };
-    in {
-      homeConfigurations = {
-        linux = homeConfig {
-          system = linuxSystem;
-          username = "gwelican";
-          homeDir = "/home/gwelican";
-        };
-        mac = homeConfig {
-          system = macSystem;
-          username = "pvarsanyi";
-          homeDir = "/Users/pvarsanyi";
-        };
-      };
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager}: let
+    configuration = {pkgs, lib, config, ...}: {
+      environment.systemPackages = [
+        pkgs.rsync
+        pkgs.fping
+        pkgs.fzf
+        pkgs.pre-commit
+        pkgs.kustomize
+        pkgs.kubectx
+        pkgs.kubernetes-helm
+        pkgs.lsd
+        pkgs.atuin
+        pkgs.starship
+        pkgs.tmux
+        pkgs.helix
+        pkgs.zoxide
+        pkgs.delta
+        pkgs.zellij
+        pkgs.bat
+        pkgs.fd
+        pkgs.jq
+        pkgs.ripgrep
+        pkgs.delta
+        pkgs.rsync
+        pkgs.go-task
+        pkgs.gcc
+        pkgs.stern
+        pkgs.sops
+        pkgs.mise
+      ];
+      nixpkgs.config.allowUnfree = true;
+      services.nix-daemon.enable = true;
+      nix.settings.experimental-features = "nix-command flakes";
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 4;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      users.users.pvarsanyi.home = /Users/pvarsanyi;
+
+      programs.direnv.enable = true;
+      # programs.home-manager.enable = true;
+      # programs.fzf.enable = true;
+      # programs.neovim.enable = true;
+      # programs.zellij.enable = true;
+      # systemd.user.startServices = "sd-switch";
     };
+  in {
+    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        # home-manager.darwinModules.home-manager
+        # {
+        #   home-manager.useGlobalPkgs = true;
+        #   home-manager.useUserPackages = true;
+        #   home-manager.users.pvarsanyi = import ./home.nix;
+        # }
+      ];
+    };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,66 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   # home.username = "pvarsanyi";
   # home.homeDirectory = "/Users/pvarsanyi";
   #
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-    pkgs.rsync
-    pkgs.fping
-    pkgs.fzf
-    pkgs.pre-commit
-    pkgs.kustomize
-    pkgs.kubectx
-    pkgs.kubernetes-helm
-    pkgs.lsd
-    pkgs.atuin
-    pkgs.starship
-    #pkgs.terraform
-    pkgs.tmux
-    pkgs.helix
-    pkgs.zoxide
-    pkgs.delta
-    pkgs.zellij
-    pkgs.bat
-    pkgs.fd
-    pkgs.jq
-    pkgs.ripgrep
-    pkgs.delta
-    pkgs.rsync
-    pkgs.go-task
-    pkgs.gcc
-    pkgs.stern
-    pkgs.sops
-
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -95,16 +41,8 @@
   };
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-  programs.direnv.enable = true;
-  programs.fzf.enable = true;
   #programs.direnv.nix-direnv.enable = true;
-  programs.neovim.enable = true;
   #programs.zsh.enable = true;
-  programs.zellij.enable = true;
   #programs.zsh.enable = true;
-
-  systemd.user.startServices = "sd-switch";
-
 
 }
