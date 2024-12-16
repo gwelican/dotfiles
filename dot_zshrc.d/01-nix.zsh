85d51a47c518fddf72a374a6930e4876c45924c5
Peter Varsanyi <superfly@gwelican.eu>
2024-12-16 10:27:02 -0800

    update

--- dot_zshrc.d/01-nix.zsh
+++ dot_zshrc.d/01-nix.zsh
@@ -2,3 +2,10 @@ if [ "$EUID" -eq 0 ];then
     source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
 fi
 fpath=(/nix/var/nix/profiles/per-user/root/profile/share/zsh/site-functions/ $fpath)
+
+
+# Nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+# End Nix
--- dot_zshrc.d/15-path.zsh
+++ dot_zshrc.d/15-path.zsh
@@ -1,3 +1,5 @@
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/.toolbox/bin:$PATH
+
+export PATH=~/.nix-profile/bin:$PATH
