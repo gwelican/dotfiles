feceb700c62e8ff3efbd943e0e67b6d86f274fdd
Peter Varsanyi <superfly@gwelican.eu>
2024-12-16 19:15:41 -0800

    delete

--- dot_zshrc.d/02-asdf.zsh
+++
@@ -1,5 +0,0 @@
-# ASDF
-export AWS_HOME=~/.aws
-
-eval "$(mise activate zsh)"
-eval "$(direnv hook zsh)"
--- dot_zshrc.d/15-path.zsh
+++
@@ -1,5 +0,0 @@
-export PATH=$HOME/.local/bin:$PATH
-export PATH=$HOME/bin:$PATH
-export PATH=$HOME/.toolbox/bin:$PATH
-
-export PATH=~/.nix-profile/bin:$PATH
