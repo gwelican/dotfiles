c18161777858301d55d5e9ece0988fb84daefc9a
Peter Varsanyi <superfly@gwelican.eu>
2025-01-02 14:26:42 -0800

    update nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -14,71 +14,71 @@
   };
 
   outputs = { self, nix-darwin, nixpkgs, home-manager, stablePkgs, nix-homebrew}@ inputs:
-  let
-    secrets = ./secrets.nix;
-    macConfiguration = {pkgs, lib, config, ...}: {
-      nixpkgs.config.allowUnfree = true;
-      services.nix-daemon.enable = true;
-      nix.settings.experimental-features = "nix-command flakes";
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 4;
-      nixpkgs.hostPlatform = "aarch64-darwin";
-      users.users.pvarsanyi.home = /Users/pvarsanyi;
+    let
+      secrets = ./secrets.nix;
+      macConfiguration = {pkgs, lib, config, ...}: {
+        nixpkgs.config.allowUnfree = true;
+        services.nix-daemon.enable = true;
+        nix.settings.experimental-features = "nix-command flakes";
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 4;
+        nixpkgs.hostPlatform = "aarch64-darwin";
+        users.users.pvarsanyi.home = /Users/pvarsanyi;
 
-      homebrew = {
-        enable = true;
-        brews = [
-          "mas"
-          "tailscale"
-          "ghostty"
-        ];
-        casks = [
-
-        ];
-        #   "hammerspoon"
-        #   "firefox"
-        #   "iina"
-        #   "the-unarchiver"
-        # ];
-        # masApps = {
-        #   "Telegram" = 747648890;
-        # };
-        onActivation.cleanup = "none";
-      };
-
-      system.defaults = {
-        dock.autohide = false;
-        NSGlobalDomain.AppleICUForce24HourTime = true;
-        NSGlobalDomain.AppleInterfaceStyle = "Dark";
-        NSGlobalDomain.KeyRepeat = 2;
-        dock.persistent-apps = [
-          "${pkgs.obsidian}/Applications/Obsidian.app"
-          "${pkgs.wezterm}/Applications/Wezterm.app"
-          "${pkgs.spotify}/Applications/Spotify.app"
-          "/Applications/Telegram.app"
-          "/Applications/Slack.app"
-          "/Applications/WezTerm.app"
-          "/Applications/Discord.app"
-          "/Applications/Messenger.app"
-        ];
-      };
-
-      programs.direnv.enable = true;
-      programs.zsh.enable = true;
-
-      # programs.home-manager.enable = true;
-      # programs.fzf.enable = true;
-      # programs.neovim.enable = true;
-      # programs.zellij.enable = true;
-      # systemd.user.startServices = "sd-switch";
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
+        homebrew = {
+          enable = true;
+          brews = [
+            "mas"
+            "tailscale"
+          ];
+          casks = [
+            "hammerspoon"
+            "ghostty"
+          ];
+          #   "hammerspoon"
+          #   "firefox"
+          #   "iina"
+          #   "the-unarchiver"
+          # ];
+          # masApps = {
+          #   "Telegram" = 747648890;
+          # };
+          onActivation.cleanup = "none";
         };
-      in
-        pkgs.lib.mkForce ''
+
+        system.defaults = {
+          dock.autohide = false;
+          NSGlobalDomain.AppleICUForce24HourTime = true;
+          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          NSGlobalDomain.KeyRepeat = 2;
+          dock.persistent-apps = [
+            "${pkgs.obsidian}/Applications/Obsidian.app"
+            "${pkgs.wezterm}/Applications/Wezterm.app"
+            "${pkgs.spotify}/Applications/Spotify.app"
+            "/Applications/Telegram.app"
+            "/Applications/Slack.app"
+            "/Applications/WezTerm.app"
+            "/Applications/Discord.app"
+            "/Applications/Messenger.app"
+          ];
+        };
+
+        programs.direnv.enable = true;
+        programs.zsh.enable = true;
+
+        # programs.home-manager.enable = true;
+        # programs.fzf.enable = true;
+        # programs.neovim.enable = true;
+        # programs.zellij.enable = true;
+        # systemd.user.startServices = "sd-switch";
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in
+          pkgs.lib.mkForce ''
         # Set up applications.
         echo "setting up ${env}/Applications..." >&2
         echo "Ok: ${pkgs.p4v}"
@@ -90,37 +90,37 @@
             echo "copying $src" >&2
             ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
             done
-            '';
+          '';
 
-      # fonts.packages = [ (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; }) ];
-    };
+        # fonts.packages = [ (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; }) ];
+      };
 
-  in {
-    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
-      modules = [
-        home-manager.darwinModules.home-manager
-        {
-          home-manager = {
-            useUserPackages = true;
-            backupFileExtension = "bak";
-            # useGlobalPkgs = true;
-            users.pvarsanyi = import ./home.nix;
-          };
-        }
-        macConfiguration
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            enableRosetta = true;
-            user = "pvarsanyi";
-            autoMigrate = true;
-          };
-        }
-        (
-          import ./pkgs.nix { upkgs = nixpkgs; spkgs = stablePkgs; architecture = "aarch64-darwin"; }
-        )
-      ];
+    in {
+      darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+        modules = [
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              backupFileExtension = "bak";
+              # useGlobalPkgs = true;
+              users.pvarsanyi = import ./home.nix;
+            };
+          }
+          macConfiguration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "pvarsanyi";
+              autoMigrate = true;
+            };
+          }
+          (
+            import ./pkgs.nix { upkgs = nixpkgs; spkgs = stablePkgs; architecture = "aarch64-darwin"; }
+          )
+        ];
+      };
     };
-  };
 }
--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,77 +1,81 @@
 { upkgs, spkgs, architecture, ...}:
 let
-stablePkgs = import spkgs {
-  system = architecture;
-  config = { allowUnfree = true; };
+  stablePkgs = import spkgs {
+    system = architecture;
+    config = { allowUnfree = true; };
 
-};
-unstablePkgs = import upkgs {
-  system = architecture;
-  config = { allowUnfree = true; };
-};
+  };
+  unstablePkgs = import upkgs {
+    system = architecture;
+    config = { allowUnfree = true; };
+  };
 in {
   environment.systemPackages = [
     stablePkgs.go-task
-      unstablePkgs.mkalias
-      unstablePkgs.rsync
-      unstablePkgs.fping
-      unstablePkgs.fzf
-      unstablePkgs.zsh
-      unstablePkgs.zsh-wd
-      unstablePkgs.zsh-z
-      unstablePkgs.pre-commit
-      unstablePkgs.kustomize
-      unstablePkgs.kubectx
-      unstablePkgs.kubernetes-helm
-      unstablePkgs.lsd
-      unstablePkgs.tailspin
-      unstablePkgs.atuin
-      unstablePkgs.starship
-      unstablePkgs.tmux
-      unstablePkgs.watchexec
-      unstablePkgs.nmap
+    unstablePkgs.mkalias
+    unstablePkgs.rsync
+    unstablePkgs.fping
+    unstablePkgs.fzf
+    unstablePkgs.zsh
+    unstablePkgs.zsh-wd
+    unstablePkgs.zsh-z
+    unstablePkgs.pre-commit
+    unstablePkgs.kustomize
+    unstablePkgs.kubectx
+    unstablePkgs.kubernetes-helm
+    unstablePkgs.lsd
+    unstablePkgs.tailspin
+    unstablePkgs.atuin
+    unstablePkgs.starship
+    unstablePkgs.tmux
+    unstablePkgs.watchexec
+    unstablePkgs.nmap
 
-      # devtools
-      unstablePkgs.mosh
-      unstablePkgs.parallel
-      unstablePkgs.mise
-      unstablePkgs.tailspin
-      unstablePkgs.ncdu
-# pkgs.helix
-      unstablePkgs.zoxide
-      unstablePkgs.delta
-      unstablePkgs.zellij
-      unstablePkgs.bat
-      unstablePkgs.fd
-      unstablePkgs.jq
-      unstablePkgs.gnupg
-      unstablePkgs.ripgrep
-      unstablePkgs.delta
-      unstablePkgs.rsync
-# pkgs.go-task
-      unstablePkgs.lazygit
-      unstablePkgs.gcc
-      unstablePkgs.stern
-      unstablePkgs.sops
-      unstablePkgs.mise
-      unstablePkgs.wget
-      unstablePkgs.bat
-      unstablePkgs.curl
-      unstablePkgs.fastfetch
-      unstablePkgs.neovim
-      unstablePkgs.parallel
-      unstablePkgs.up
+    # devtools
+    unstablePkgs.mosh
+    unstablePkgs.parallel
+    unstablePkgs.mise
+    unstablePkgs.tailspin
+    unstablePkgs.ncdu
+    # pkgs.helix
+    unstablePkgs.zoxide
+    unstablePkgs.delta
+    unstablePkgs.zellij
+    unstablePkgs.bat
+    unstablePkgs.fd
+    unstablePkgs.jq
+    unstablePkgs.gnupg
+    unstablePkgs.ripgrep
+    unstablePkgs.delta
+    unstablePkgs.rsync
+    # pkgs.go-task
+    unstablePkgs.lazygit
+    unstablePkgs.gcc
+    unstablePkgs.stern
+    unstablePkgs.sops
+    unstablePkgs.mise
+    unstablePkgs.wget
+    unstablePkgs.bat
+    unstablePkgs.curl
+    unstablePkgs.fastfetch
+    unstablePkgs.neovim
+    unstablePkgs.parallel
+    unstablePkgs.up
 
-      # gui
-      unstablePkgs.p4v
-      unstablePkgs.openscad
-      unstablePkgs.karabiner-elements
+    # gui
+    unstablePkgs.p4v
+    # unstablePkgs.openscad
+    unstablePkgs.karabiner-elements
 
-      unstablePkgs.vscode
-      unstablePkgs.discord
-      unstablePkgs.wezterm
-      unstablePkgs.spotify
-      unstablePkgs.p4v
-      unstablePkgs.obsidian
-      ];
+    unstablePkgs.fira-code
+    unstablePkgs.vscode
+    unstablePkgs.discord
+    unstablePkgs.topgrade
+    unstablePkgs.glow
+    # unstablePkgs.ghostty
+    unstablePkgs.wezterm
+    unstablePkgs.spotify
+    unstablePkgs.p4v
+    unstablePkgs.obsidian
+  ];
 }
