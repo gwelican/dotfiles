6a2aafba03df23045c8c1192cfe5cc7ecb92f50b
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2025-06-26 18:25:16 -0700

    fix dock apps

--- nix/hosts/common/darwin-common-dock.nix
+++ nix/hosts/common/darwin-common-dock.nix
@@ -2,15 +2,11 @@
 {
    system.defaults.dock = {
     persistent-apps = [
-      "/Applications/Google Chrome.app"
-      "/Applications/Firefox.app"
+      "/Applications/Brave Browser.app"
       "/Applications/Telegram.app"
-      "/Applications/Signal.app"
       "/Applications/Discord.app"
-      "/Applications/Slack.app"
-      "/Applications/Ivory.app"
+      # "/Applications/Slack.app"
       "/Applications/Obsidian.app"
-      "/Applications/Fantastical.app"
       "/Applications/Visual Studio Code.app"
       "/Applications/Spotify.app"
       "/Applications/Plexamp.app"
