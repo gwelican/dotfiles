58ba9c8494035dae0b0857216b4beeac07996e20
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2026-04-09 10:18:07 -0700

    use bun for omp

--- dot_zshrc.d/03-path.zsh
+++ dot_zshrc.d/03-path.zsh
@@ -2,5 +2,6 @@ export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/bin:$PATH
 
 export PATH=~/.nix-profile/bin:$PATH
+export PATH=~/.bun/bin:$PATH
 
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
